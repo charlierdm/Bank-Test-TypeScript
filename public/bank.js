071e1e805c76dc5ff8acd70955b2770b38602452
Charlie <charlie.davismiller@gmail.com>
2021-10-21 14:35:26 +0100

    added date property to Transaction class and new Date argument per transaction

--- public/bank.js
+++ public/bank.js
@@ -1,18 +1,19 @@
 class Transaction {
-    constructor(credit, debit, balance) {
+    constructor(date, credit, debit, balance) {
+        this.date = date;
         this.credit = credit;
         this.debit = debit;
         this.balance = balance;
     }
 }
-class Account {
+export class Account {
     constructor(balance = 0) {
         this.deposit = (amount, transaction = Transaction) => {
-            let newDeposit = new transaction(amount, 0, this.balance += amount);
+            let newDeposit = new transaction(new Date(), amount, 0, this.balance += amount);
             this.transactionHistory.push(newDeposit);
         };
         this.withdraw = (amount, transaction = Transaction) => {
-            let newWithdraw = new transaction(0, amount, this.balance -= amount);
+            let newWithdraw = new transaction(new Date(), 0, amount, this.balance -= amount);
             this.transactionHistory.push(newWithdraw);
         };
         this.balance = balance;
